26b4964ce474aba2e128e67e56d2dad9b574fce2
Oğuzhan <oguzhnural@gmail.com>
2025-01-10 14:38:57 +0300

    Add jest config.ts

---
+++ backend/jest.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.ts'],
+  verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/__tests__/**'
+  ],
+  coverageDirectory: 'coverage'
+};
+
+export default config; 
